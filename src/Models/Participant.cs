dee18718637429f8d952044534b1b7153cad4c3c
Sintayu-de-Kuiper <sintayudekuiper@gmail.com>
2024-03-28 09:38:12 +0100

    Added and implemented a 'Participant' class

---
+++ src/Models/Participant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack_DealersPerspective.Models
+{
+    public class Participant
+    {
+        public EventHandler<ParticipantActionEventArgs>? Hit;
+        public EventHandler<ParticipantActionEventArgs>? Stand;
+
+        public void PerformHit(Hand hand)
+        {
+            OnHit(hand);
+        }
+
+        public void PerformStand(Hand hand)
+        {
+            OnStand(hand);
+        }
+
+        protected virtual void OnHit(Hand hand)
+        {
+            Hit?.Invoke(this, new ParticipantActionEventArgs(hand));
+        }
+
+        protected virtual void OnStand(Hand hand)
+        {
+            Stand?.Invoke(this, new ParticipantActionEventArgs(hand));
+        }
+    }
+}
---
+++ src/Models/ParticipantActionEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack_DealersPerspective.Models
+{
+    public class ParticipantActionEventArgs(Hand hand) : EventArgs
+    {
+        public Hand Hand { get; } = hand;
+    }
+}

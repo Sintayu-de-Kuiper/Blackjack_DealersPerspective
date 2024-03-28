297ac416cacaee7bd4efd30aed8fa8a2bb26a882
Sintayu-de-Kuiper <sintayudekuiper@gmail.com>
2024-03-28 09:10:04 +0100

    Added and implemented a 'Chip' class.

---
+++ src/Models/Chip.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack_DealersPerspective.Models
+{
+    public class Chip(Chip.VALUE value)
+    {
+        public enum VALUE
+        {
+            ONE = 1,
+            FIVE = 5,
+            TWENTYFIVE = 25,
+            HUNDRED = 100,
+            FIVEHUNDRED = 500,
+            THOUSAND = 1_000
+        }
+
+        public VALUE Value { get; private set; } = value;
+    }
+}
